a82e1b629ffa50a7c86ef9ff9e0b273dd0990530
joshyam-k <yamamojo@reed.edu>
2021-04-20 19:30:54 -0700

    some more cool functionality

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -6,18 +6,36 @@ library(sf)
 library(scales)
 library(glue)
 library(shinyWidgets)
-library(dashboardthemes)
-library(extrafont)
+library(fresh)
 library(plotly)
+
+## non-reactive data loading ---------------------------------------------------
 
 data_full <- readr::read_csv("https://raw.githubusercontent.com/joshyam-k/scheduled-commit-action/master/data-raw/lime.csv")
 map <- st_read('/Users/joshuayamamoto/test/ds_dash/shiny_app/sf_boundary1.shp')
 top <- max(data_full$last_updated)
 top <- with_tz(top, "America/Los_Angeles")
 
-font <- list(family = "Raleway",
-             size = 10,
-             color = "black")
+## fresh theme -----------------------------------------------------------------
+
+mytheme <- create_theme(
+  adminlte_color(
+    light_blue = "#f5f7f5"
+  ),
+  adminlte_sidebar(
+    width = "400px",
+    dark_bg = "#6a7d72",
+    dark_hover_bg = "#e1e8e4",
+    dark_color = "#0ca84f"
+  ),
+  adminlte_global(
+    content_bg = "#FFF",
+    box_bg = "D8DEE9",
+    info_box_bg = "D8DEE9"
+  )
+)
+
+##--------------------------------------------------------------------------------
 
 data_full <- data_full %>% 
   separate(
@@ -38,15 +56,19 @@ data_full <- data_full %>%
 
 
 ui <- dashboardPage(
-  dashboardHeader(),
+  dashboardHeader(
+    title = "test"
+    ),
   dashboardSidebar(),
   dashboardBody(
-    shinyDashboardThemes(
-      theme = "grey_light"
-    ),
+    use_theme(mytheme),
     fluidRow(
-      box(plotlyOutput("plot1"), width = 9),
-      box(knobInput(
+      box(
+        solidHeader = T,
+        plotOutput("plot1"), width = 8),
+      box(
+        solidHeader = T,
+        knobInput(
           inputId = "Knob",
           label = "Number of hours ago:",
           value = 0,
@@ -57,10 +79,16 @@ ui <- dashboardPage(
           fgColor = "#60c957",
           inputColor = "#60c957",
           pre = "-"
-          ), width = 3
+          ), width = 4
         )
-      )
+      ),
+    fluidRow(
+      box(
+        SolidHeader = T,
+        plotOutput("plot2"), width = 8
+        )
     )
+  )
 )
 
 
@@ -76,11 +104,17 @@ server <- function(input, output) {
       filter(full_date == date)
     
   })   
+
   
   viewed <- reactive({
     date <- max(data_full$full_date)
     hour(date) <- hour(date) - input$Knob 
     date
+  })
+  
+  full_with_marker <- reactive({
+    data_full %>% 
+      mutate(selected = if_else(full_date == viewed(), 1, 0))
   })
   
   plot_static <- reactive({
@@ -89,54 +123,57 @@ server <- function(input, output) {
       geom_hex(data = filt_data(), aes(lon, lat), inherit.aes = F, bins = 18) +
       geom_sf(fill = NA, color = "grey50") +
       scale_fill_gradient(
-        #name = "  Number\n of free\n Scooters",
         low = "white",
         high = "#60c957"
       ) +
       theme_void() +
       labs(
         caption = glue("Last updated: {top}"),
-        title = glue('  Number of free   \n scooters on \n {format(viewed(), "%B %d, at %H:%M %p")}')
+        fill = glue('  Number of free   \n scooters on \n {format(viewed(), "%B %d, at %H:%M %p")}')
       ) +
       theme(
-        plot.caption = element_text(family = "Roboto Mono", hjust = 1.3, size = 12 ),
-        #legend.position = "left",
-        #legend.key.size = unit(1, "cm"),
-        #legend.text = element_text(size = 8, family = "Roboto Mono"),
-        #legend.title = element_text(size = 15, family = "Roboto Mono")
+        plot.caption = element_text(family = "Roboto Mono", hjust = 1, size = 12 ),
+        legend.position = "left",
+        legend.key.size = unit(1, "cm"),
+        legend.text = element_text(size = 8, family = "Roboto Mono"),
+        legend.title = element_text(size = 15, family = "Roboto Mono")
       ) +
       guides(fill = guide_colorbar(title.position = "left"))
     
   })
   
-  output$plot1 <- renderPlotly({
+  output$plot1 <- renderPlot({
   
-    ggplotly(plot_static(),
-             dynamicTicks = TRUE) %>% 
-      layout(
-        xaxis = list(
-          title = "",
-          zeroline = FALSE,
-          showline = FALSE,
-          showticklabels = FALSE,
-          showgrid = FALSE
-        ),
-        yaxis = list(
-          title = "",
-          zeroline = FALSE,
-          showline = FALSE,
-          showticklabels = FALSE,
-          showgrid = FALSE
-        ),
-        font = list(
-          family = "h"
-        )
-      ) %>% 
-      config(displayModeBar = F) 
+    plot_static()
     
   })
   
+  output$plot2 <- renderPlot({
+    
+    full_with_marker() %>% 
+      count(full_date) %>% 
+      ggplot(aes(x = full_date, y = n, fill = ifelse(full_date == viewed(), "yes", "no"))) +
+      geom_col(show.legend = F) +
+      scale_fill_manual(
+        values = c("#344463","#8ca4d4")
+      ) +
+      labs(
+        x = "Time",
+        y = 'Count'
+      ) +
+      theme_minimal() +
+      theme(
+        axis.title.x = element_text(family = "Roboto Mono"),
+        axis.title.y = element_text(family = "Roboto Mono"),
+        axis.text.x = element_text(family = "Roboto Mono"),
+        axis.text.y = element_text(family = "Roboto Mono")
+      ) 
+    
+  }, height = 200)
+  
 
+  
+  
 }
 
 shinyApp(ui, server)
